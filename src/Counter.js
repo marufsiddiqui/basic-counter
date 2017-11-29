a05a7a66ffb8394d7de0631afd1ac12d63a4feb0
Maruf Siddiqui <maruf.siddiqui@shore.com>
2017-11-29 17:17:55 +0100

    Add counter functionality

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
 export default class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter : 0
+    };
+    this.inc = this.inc.bind(this);
+    this.dec = this.dec.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  inc() {
+    console.log('hi');
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  dec() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
+  reset() {
+    this.setState({
+      counter: 0
+    });
+  }
   render() {
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {this.state.counter}</h1>
+        <button onClick={this.inc} className="full-width">Increment</button>
+        <button onClick={this.dec} className="full-width">Decrement</button>
+        <button onClick={this.reset} className="full-width">Reset</button>
       </section>
     );
   }
